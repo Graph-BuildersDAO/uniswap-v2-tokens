f0d5db77b1e1bb4e3d7e9a805728ca56a3240b70
Data-Nexus <derek.m.meyer@gmail.com>
2024-02-14 15:42:47 -0500

    Loop instead of dump

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -6,6 +6,7 @@ import { Pair as PairTemplate } from '../types/templates'
 import {
   convertTokenToDecimal,
   FACTORY_ADDRESS,
+  factoryContract,
   fetchTokenDecimals,
   fetchTokenName,
   fetchTokenSymbol,
@@ -160,132 +161,136 @@ export function handleHydrateStore(block: ethereum.Block): void {
     bundle.save()
   }
 
-  factory.pairCount = factory.pairCount + 1
-  factory.save()
+  let pairCount = factory.pairCount
+  for (let i = 0; i < 1000; i++) {
+    let index = BigInt.fromI32(i).plus(BigInt.fromI32(pairCount))
 
-  //Start the big loop
-  let pairs = PairBasic.getExistingPairs()
+    let pairAddress = factoryContract.try_allPairs(index)
 
-  for (let i = 0; i < pairs.length; i++) {
-    let newPair = pairs[i]
+    if (!pairAddress.reverted) {
+      let pairContract = PairContract.bind(pairAddress)
 
-    factory.pairCount = factory.pairCount + 1
-    factory.save()
+      let token0_id = pairContract.try_token0()
+      let token1_id = pairContract.try_token1()
 
-    // create the tokens
-    let token0 = Token.load(newPair.token_0)
-    let token1 = Token.load(newPair.token_1)
+      // create the tokens
+      let token0 = Token.load(token0_id)
+      let token1 = Token.load(token1_id)
 
-    // fetch info if null
-    if (!token0) {
-      token0 = new Token(newPair.token_0)
-      token0.symbol = fetchTokenSymbol(Address.fromString(newPair.token_0))
-      token0.name = fetchTokenName(Address.fromString(newPair.token_0))
-      token0.totalSupply = fetchTokenTotalSupply(Address.fromString(newPair.token_0))
-      let decimals = fetchTokenDecimals(Address.fromString(newPair.token_0))
+      // fetch info if null
+      if (!token0) {
+        token0 = new Token(token0_id)
+        token0.symbol = fetchTokenSymbol(Address.fromString(token0_id))
+        token0.name = fetchTokenName(Address.fromString(token0_id))
+        token0.totalSupply = fetchTokenTotalSupply(Address.fromString(token0_id))
+        let decimals = fetchTokenDecimals(Address.fromString(token0_id))
 
-      // bail if we couldn't figure out the decimals
-      if (!decimals) {
-        log.debug('mybug the decimal on token 0 was null', [])
-        return
+        // bail if we couldn't figure out the decimals
+        if (!decimals) {
+          log.debug('mybug the decimal on token 0 was null', [])
+          return
+        }
+
+        token0.decimals = decimals
+        token0.derivedETH = ZERO_BD
+        token0.tradeVolume = ZERO_BD
+        token0.tradeVolumeUSD = ZERO_BD
+        token0.untrackedVolumeUSD = ZERO_BD
+        token0.totalLiquidity = ZERO_BD
+        token0.lastMinuteArchived = BigInt.fromI32(0)
+        token0.lastHourArchived = BigInt.fromI32(0)
+        token0.lastMinuteRecorded = BigInt.fromI32(0)
+        token0.lastHourRecorded = BigInt.fromI32(0)
+        token0.minuteArray = []
+        token0.hourArray = []
+        // token0.allPairs = []
+        token0.txCount = ZERO_BI
       }
 
-      token0.decimals = decimals
-      token0.derivedETH = ZERO_BD
-      token0.tradeVolume = ZERO_BD
-      token0.tradeVolumeUSD = ZERO_BD
-      token0.untrackedVolumeUSD = ZERO_BD
-      token0.totalLiquidity = ZERO_BD
-      token0.lastMinuteArchived = BigInt.fromI32(0)
-      token0.lastHourArchived = BigInt.fromI32(0)
-      token0.lastMinuteRecorded = BigInt.fromI32(0)
-      token0.lastHourRecorded = BigInt.fromI32(0)
-      token0.minuteArray = []
-      token0.hourArray = []
-      // token0.allPairs = []
-      token0.txCount = ZERO_BI
+      // fetch info if null
+      if (!token1) {
+        token1 = new Token(token1_id)
+        token1.symbol = fetchTokenSymbol(Address.fromString(token1_id))
+        token1.name = fetchTokenName(Address.fromString(token1_id))
+        token1.totalSupply = fetchTokenTotalSupply(Address.fromString(token1_id))
+        let decimals = fetchTokenDecimals(Address.fromString(token1_id))
+
+        // bail if we couldn't figure out the decimals
+        if (!decimals) {
+          return
+        }
+        token1.decimals = decimals
+        token1.derivedETH = ZERO_BD
+        token1.tradeVolume = ZERO_BD
+        token1.tradeVolumeUSD = ZERO_BD
+        token1.untrackedVolumeUSD = ZERO_BD
+        token1.totalLiquidity = ZERO_BD
+        token1.lastMinuteArchived = BigInt.fromI32(0)
+        token1.lastHourArchived = BigInt.fromI32(0)
+        token1.lastMinuteRecorded = BigInt.fromI32(0)
+        token1.lastHourRecorded = BigInt.fromI32(0)
+        token1.minuteArray = []
+        token1.hourArray = []
+        // token1.allPairs = []
+        token1.txCount = ZERO_BI
+      }
+
+      let pair = Pair.load(pairAddress.value.toHexString())
+      if (!pair) {
+        pair = new Pair(pairAddress.value.toHexString())
+        pair.token0 = token0.id
+        pair.token1 = token1.id
+        pair.liquidityProviderCount = ZERO_BI
+        pair.createdAtTimestamp = block.timestamp
+        pair.createdAtBlockNumber = block.number
+        pair.txCount = ZERO_BI
+        pair.reserve0 = ZERO_BD
+        pair.reserve1 = ZERO_BD
+        pair.trackedReserveETH = ZERO_BD
+        pair.reserveETH = ZERO_BD
+        pair.reserveUSD = ZERO_BD
+        pair.totalSupply = ZERO_BD
+        pair.volumeToken0 = ZERO_BD
+        pair.volumeToken1 = ZERO_BD
+        pair.volumeUSD = ZERO_BD
+        pair.untrackedVolumeUSD = ZERO_BD
+        pair.token0Price = ZERO_BD
+        pair.token1Price = ZERO_BD
+
+        pair.save()
+
+        pairCount = pairCount + 1
+
+        let token0Contract = ERC20.bind(Address.fromString(token0.id))
+        let token1Contract = ERC20.bind(Address.fromString(token0.id))
+
+        let try_balance0 = token0Contract.try_balanceOf(Address.fromString(pair.id))
+        let try_balance1 = token1Contract.try_balanceOf(Address.fromString(pair.id))
+
+        if (!try_balance0.reverted) {
+          token0.totalLiquidity = token0.totalLiquidity.plus(convertTokenToDecimal(try_balance0.value, token0.decimals))
+        }
+
+        if (!try_balance1.reverted) {
+          token1.totalLiquidity = token1.totalLiquidity.plus(convertTokenToDecimal(try_balance1.value, token0.decimals))
+        }
+
+        let pairLookup0 = new PairTokenLookup(token0.id.concat('-').concat(token1.id))
+        pairLookup0.pair = pair.id
+        pairLookup0.save()
+
+        let pairLookup1 = new PairTokenLookup(token1.id.concat('-').concat(token0.id))
+        pairLookup1.pair = pair.id
+        pairLookup1.save()
+
+        PairTemplate.create(Address.fromString(pair.id))
+      }
+
+      token0.save()
+      token1.save()
     }
-
-    // fetch info if null
-    if (!token1) {
-      token1 = new Token(newPair.token_1)
-      token1.symbol = fetchTokenSymbol(Address.fromString(newPair.token_1))
-      token1.name = fetchTokenName(Address.fromString(newPair.token_1))
-      token1.totalSupply = fetchTokenTotalSupply(Address.fromString(newPair.token_1))
-      let decimals = fetchTokenDecimals(Address.fromString(newPair.token_1))
-
-      // bail if we couldn't figure out the decimals
-      if (!decimals) {
-        return
-      }
-      token1.decimals = decimals
-      token1.derivedETH = ZERO_BD
-      token1.tradeVolume = ZERO_BD
-      token1.tradeVolumeUSD = ZERO_BD
-      token1.untrackedVolumeUSD = ZERO_BD
-      token1.totalLiquidity = ZERO_BD
-      token1.lastMinuteArchived = BigInt.fromI32(0)
-      token1.lastHourArchived = BigInt.fromI32(0)
-      token1.lastMinuteRecorded = BigInt.fromI32(0)
-      token1.lastHourRecorded = BigInt.fromI32(0)
-      token1.minuteArray = []
-      token1.hourArray = []
-      // token1.allPairs = []
-      token1.txCount = ZERO_BI
-    }
-
-    let pair = Pair.load(newPair.id)
-    if (!pair) {
-      pair = new Pair(newPair.id)
-      pair.token0 = token0.id
-      pair.token1 = token1.id
-      pair.liquidityProviderCount = ZERO_BI
-      pair.createdAtTimestamp = BigInt.fromI32(newPair.created_at_timestamp)
-      pair.createdAtBlockNumber = BigInt.fromI32(newPair.created_at_timestamp)
-      pair.txCount = ZERO_BI
-      pair.reserve0 = ZERO_BD
-      pair.reserve1 = ZERO_BD
-      pair.trackedReserveETH = ZERO_BD
-      pair.reserveETH = ZERO_BD
-      pair.reserveUSD = ZERO_BD
-      pair.totalSupply = ZERO_BD
-      pair.volumeToken0 = ZERO_BD
-      pair.volumeToken1 = ZERO_BD
-      pair.volumeUSD = ZERO_BD
-      pair.untrackedVolumeUSD = ZERO_BD
-      pair.token0Price = ZERO_BD
-      pair.token1Price = ZERO_BD
-
-      pair.save()
-
-      let token0Contract = ERC20.bind(Address.fromString(token0.id))
-      let token1Contract = ERC20.bind(Address.fromString(token0.id))
-
-      let try_balance0 = token0Contract.try_balanceOf(Address.fromString(pair.id))
-      let try_balance1 = token0Contract.try_balanceOf(Address.fromString(pair.id))
-
-      if (!try_balance0.reverted) {
-        token0.totalLiquidity = token0.totalLiquidity.plus(convertTokenToDecimal(try_balance0.value, token0.decimals))
-      }
-
-      if (!try_balance1.reverted) {
-        token1.totalLiquidity = token1.totalLiquidity.plus(convertTokenToDecimal(try_balance1.value, token0.decimals))
-      }
-
-      let pairLookup0 = new PairTokenLookup(newPair.token_0.concat('-').concat(newPair.token_1))
-      pairLookup0.pair = pair.id
-      pairLookup0.save()
-
-      let pairLookup1 = new PairTokenLookup(newPair.token_1.concat('-').concat(newPair.token_0))
-      pairLookup1.pair = pair.id
-      pairLookup1.save()
-
-      PairTemplate.create(Address.fromString(pair.id))
-    }
-
-    token0.save()
-    token1.save()
   }
 
+  factory.pairCount = pairCount
   factory.save()
 }
